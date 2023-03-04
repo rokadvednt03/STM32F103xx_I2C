881ba42bed8e6f5740d7926457620a9d2a5361f8
ROKAD VEDANT <rokadvedant03@gmail.com>
2023-03-05 01:04:34 +0530

    i2c_mastersend_done

--- test.c
+++ test.c
@@ -10,6 +10,8 @@ int main()
 	uint8_t data[] = "Hello_I2C-THIS IS VEDANT ROKAD";
 	uint8_t *pdata = data;
 	uint32_t len = strlen((char*)data);
+	I2C_Handle_t i2ctest;
+	
 	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN ;
 	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN ;
 	
@@ -19,41 +21,17 @@ int main()
 	
 	GPIOB->CRL |= (GPIO_CRL_MODE7_0) | (GPIO_CRL_MODE7_1);
 	GPIOB->CRL |= (GPIO_CRL_CNF7_0) | (GPIO_CRL_CNF7_1);
+	
 		
-	I2C1->CR2 |= (0x24 & (0x3F));
-	I2C1->CCR |= (0x32 & (0xFFF));
-	I2C1->TRISE = 0x25;
+	i2ctest.pI2Cx = I2C1;
+	i2ctest.i2cconfig.ACKControl = I2C_ACK_ENABLE;
+	i2ctest.i2cconfig.DeviceADDR = 0x55;
+	i2ctest.i2cconfig.SCLSpeed = 50;
+	I2C_Init(&i2ctest);
 	
 	I2C1->CR1 |= (I2C_CR1_PE);
 	
-	
-	I2C1->CR1 |= I2C_CR1_START;
-	
-	while(!(I2C1->SR1 & I2C_SR1_SB));
-	
-	reg_state = I2C1->SR1;
-
-	I2C1->DR = slave_addr;
-	
-	while(!(I2C1->SR1 & I2C_SR1_ADDR));
-	
-	reg_state = I2C1->SR1;
-	reg_state = I2C1->SR2;
-	(void)reg_state;
-		
-	while(!(I2C1->SR1 & I2C_SR1_TXE));
-	
-	while(len--)
-	{
-		I2C1->DR = ((*pdata) & (0xFF)) ;
-		while(!(I2C1->SR1 & I2C_SR1_TXE));
-		pdata++;
-		//I2C1->CR1 |= I2C_CR1_START;
-	}
-	while(!(I2C1->SR1 & I2C_SR1_TXE));
-	while(!(I2C1->SR1 & I2C_SR1_BTF));
-	
-	I2C1->CR1 |= (I2C_CR1_STOP);
+	I2C_MasterSendData(&i2ctest,data,len,0x34);
 	
 }
 
